94374d227b0c520f09d586042a7ddc505c20a0bd
dastarruer <ayushpramanik399@gmail.com>
2025-03-09 19:50:36 -0400

    spicetify pywal theme

--- rofi/.config/rofi/scripts/switch_theme.sh
+++ rofi/.config/rofi/scripts/switch_theme.sh
@@ -13,26 +13,22 @@ echo "${CHOICE// /-}" | tr '[:upper:]' '[:lower:]' > "$HOME/.current_theme"
 BTOP=""
 WAL=""
 OBSIDIAN=""
-SPICETIFY="" 
 
 case $CHOICE in
     "Catppuccin")
         BTOP="catppuccin"
         WAL="$HOME/.config/wal/colorschemes/catppuccin-mocha.json"
         OBSIDIAN="Catppuccin"
-        SPICETIFY="catppuccin"
         ;;
     "Gruvbox")
         BTOP="gruvbox"
         WAL="base16-gruvbox-hard"
         OBSIDIAN="Material Gruvbox"
-        SPICETIFY="gruvbox"
         ;;
     "Rose Pine")
         BTOP="catppuccin"
         WAL="$HOME/.config/wal/colorschemes/rose-pine.json"
         OBSIDIAN="rose-pine"
-        SPICETIFY="rose-pine"
         ;;
     *)
         exit 1 # Exit if the user selects nothing or closes rofi
@@ -57,7 +53,6 @@ notify-send -t 500 "Changing wallpaper..."
 
 # Update spicetify (spotify)  
 notify-send -t 500 "Updating Spotify..."
-$HOME/.spicetify/spicetify config color_scheme $SPICETIFY  # For some reason i gotta specify spicetify's exact path'
 $HOME/.spicetify/spicetify apply
 
 # Update Obsidian  
